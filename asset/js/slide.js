7daf752badb7b3df185d35a01005056ffc5814f1
wander217 <Trinhthinh2171999@gmail.com>
2021-07-26 18:50:14 +0700

    final

--- asset/js/slide.js
+++ asset/js/slide.js
@@ -3,12 +3,10 @@ $(document).ready(function(){
       dots: true,
       infinite: true,
       arrows: false,
-      speed: 300,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
-      centerMode: true,
-      variableWidth: true,
+      variableWidth: true
     };
     $(".blog-slider").slick(data);
   });
